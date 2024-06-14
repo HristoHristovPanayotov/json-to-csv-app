d6e34816fb5adb837ac3c3bad0116c690278f967
HristoHristovPanayotov <emailformyself123123@gmail.com>
2024-06-14 08:50:33 +0300

    json-to-csv-app

---
+++ json-to-csv-app/json-to-csv-app/api-paths.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace json_to_csv_app
+{
+    public enum ApiPaths
+    {
+        Name,
+        Region,
+        Subregion,
+        Capital,
+    }
+}
